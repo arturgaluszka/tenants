2bf4e0e45813c977f587ab313a450a9d666751c3
Pawel Kalecinski <pawelkalecinski94@gmail.com>
2015-12-05 19:57:23 +0100

    [i01t01]Stworzenie Menu umożliwiającego wybór funkcjonalności

--- app/src/main/java/com/tenantsproject/flatmates/MainActivity.java
+++ app/src/main/java/com/tenantsproject/flatmates/MainActivity.java
@@ -1,13 +1,34 @@
 package com.tenantsproject.flatmates;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b;
+    Button b2;
+    Button b3;
+    Button b4;
+    Button b5;
+    Button b6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b = (Button) findViewById(R.id.button);
+        b2 = (Button) findViewById(R.id.button2);
+        b3 = (Button) findViewById(R.id.button3);
+        b4 = (Button) findViewById(R.id.button4);
+        b5 = (Button) findViewById(R.id.button5);
+        b6 = (Button) findViewById(R.id.button6);
+    }
+
+    public void Exit(View V){
+        System.exit(0);
     }
 }
